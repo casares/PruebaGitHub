7a4043dc08a8b2f569f3c682ee58f228034c318c
casares <cjcasares@gmail.com>
2015-06-05 18:55:54 +0200

    Segundo Commit

--- PruebaGitHub/src/com/github/ProbandoGitHub.java
+++ PruebaGitHub/src/com/github/ProbandoGitHub.java
@@ -4,6 +4,7 @@ public class ProbandoGitHub {
 
 	public static void main(String[] args) {
 		System.out.println("Clase: Probando GitHub - Primer commit");
+		System.out.println("Una modificacion - Segundo commit");
 
 	}
 
